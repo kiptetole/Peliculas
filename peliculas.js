4b370015e8059ad1578c00b71be9a6a16696f515
Jose Notario <jonomi97@gmail.com>
2020-01-17 14:23:11 +0100

    redimensionando los posters

--- peliculas.js
+++ peliculas.js
@@ -17,7 +17,7 @@ function mostrarPeliculas(){
 // Funcion que crea las card donde aparece la imagen de la pelicula y el titulo.
 function Card(objeto){
     divG = $("<div class=\'col\'></div>")
-    divP = $("<div class=\"card mt-3\" style=\"width: 25rem;\"></div>")
+    divP = $("<div class=\"card mt-3\" style=\"width: 22rem;\"></div>")
     divP.id = objeto.imdbID
     divP.append("<img class=\"card-img-top\" src="+ objeto.Poster +" alt=\"Card image cap\" style=\"height: 25rem;\">")
     divS = $("<div class=\"card-body\"></div>")
